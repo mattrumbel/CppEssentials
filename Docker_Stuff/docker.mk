9e06ec24605bd7df732ab7f079d9d37cadacb910
Matthew Rumbel <matthewrumbel@macbook-pro.attlocal.net>
2020-08-09 18:35:32 -0500

    Version numbers

--- Docker_Stuff/docker.mk
+++ Docker_Stuff/docker.mk
@@ -1,13 +1,13 @@
 .PHONY: run build update push
 
 run:
-	docker run -it --rm mattrumbel/cpp_essentials:1.0
+	docker run -it --rm mattrumbel/cpp_essentials:1.1
 
 build:
-	docker build -t mattrumbel/cpp_essentials:1.0 .
+	docker build -t mattrumbel/cpp_essentials:1.1 .
 
 update:
-	docker build -t mattrumbel/cpp_essentials:1.0 .
+	docker build -t mattrumbel/cpp_essentials:1.1 .
 
 push:
-	docker push mattrumbel/cpp_essentials:1.0
+	docker push mattrumbel/cpp_essentials:1.1
