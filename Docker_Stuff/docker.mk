25fc40228704db73b507060cd5ccfb1791ebcfef
Matthew Rumbel <matthewrumbel@macbook-pro.attlocal.net>
2020-08-16 14:16:00 -0500

    make updated

--- Docker_Stuff/docker.mk
+++ Docker_Stuff/docker.mk
@@ -1,13 +1,18 @@
+IMAGE = mattrumbel/cpp_essentials
+TAG = 1.1
+CONTAINER_NAME = cpp_container
+
+
 .PHONY: run build update push
 
 run:
-	docker run -it --name cpp_container -v ~/cpp_container_host:/host_vol --rm mattrumbel/cpp_essentials:1.1
+	docker run -it --name ${CONTAINER_NAME} -v ~/cpp_container_host:/host_vol --rm ${IMAGE}:${TAG}
 
 build:
-	docker build -t mattrumbel/cpp_essentials:1.1 .
+	docker build -t ${IMAGE}:${TAG} .
 
 update:
-	docker build -t mattrumbel/cpp_essentials:1.1 .
+	docker build -t ${IMAGE}:${TAG} .
 
 push:
-	docker push mattrumbel/cpp_essentials:1.1
+	docker push ${IMAGE}:${TAG}
