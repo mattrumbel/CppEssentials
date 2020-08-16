371eeed4e520a97d994973a3a9aa21e7d3f3dd30
Matthew Rumbel <matthewrumbel@macbook-pro.attlocal.net>
2020-08-16 14:07:45 -0500

    Docker makefile updated

--- Docker_Stuff/docker.mk
+++ Docker_Stuff/docker.mk
@@ -1,7 +1,7 @@
 .PHONY: run build update push
 
 run:
-	docker run -it --rm mattrumbel/cpp_essentials:1.1
+	docker run -it --name cpp_container -v ~/cpp_container_host:/host_vol --rm mattrumbel/cpp_essentials:1.1
 
 build:
 	docker build -t mattrumbel/cpp_essentials:1.1 .
